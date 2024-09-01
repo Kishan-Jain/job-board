2f6212e58cbd5139c922e903f7b8910e8a6abdd3
Kishan jain <jainshab7000@gmail.com>
2024-09-01 22:00:26 +0530

    start connection controller

--- job_board-project/backend/src/controllers/connections.controllers.js
+++ job_board-project/backend/src/controllers/connections.controllers.js
@@ -5,10 +5,9 @@
  * breack connection
  */
 
-import AsyncHandler from "../utils/AsyncHandler.js"
-import ApiError from "../utils/ApiError.js"
-import ApiResponce from "../utils/ApiResponce.js"
-
+import AsyncHandler from "../utils/AsyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponce from "../utils/ApiResponce.js";
 
 export const checkConnectionStatus = AsyncHandler(async (req, res) => {
   /**
@@ -17,7 +16,39 @@ export const checkConnectionStatus = AsyncHandler(async (req, res) => {
    * check secondUserId availabel in user connection array
    * return message with responce
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+    
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 export const sendConnectionRequest = AsyncHandler(async (req, res) => {
   /**
@@ -28,7 +59,38 @@ export const sendConnectionRequest = AsyncHandler(async (req, res) => {
    * set first user id in second user connectoin request array for responce
    * return message with responce
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 export const responceConnection = AsyncHandler(async (req, res) => {
   /**
@@ -40,7 +102,38 @@ export const responceConnection = AsyncHandler(async (req, res) => {
    * remove first user id from second user connection request array and set first user id in second user connectoin array with connected
    * return message with responce
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 export const breackConnection = AsyncHandler(async (req, res) => {
   /**
@@ -51,7 +144,38 @@ export const breackConnection = AsyncHandler(async (req, res) => {
    * remove userIds from user connection array
    * return message with responce
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 /**
  * check following
@@ -66,9 +190,40 @@ export const checkFollowing = AsyncHandler(async (req, res) => {
    * check user is login
    * check secondUserId received from params
    * check second userId in user following array
-   * 
+   *
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 export const checkFollowers = AsyncHandler(async (req, res) => {
   /**
@@ -77,7 +232,38 @@ export const checkFollowers = AsyncHandler(async (req, res) => {
    * check second userId in user followers array
    * return message
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 export const startFollowing = AsyncHandler(async (req, res) => {
   /**
@@ -86,7 +272,38 @@ export const startFollowing = AsyncHandler(async (req, res) => {
    * set secondUserId in user following array and firstUserId in user followers array
    * return massage and responce
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 export const stopFollowing = AsyncHandler(async (req, res) => {
   /**
@@ -96,7 +313,38 @@ export const stopFollowing = AsyncHandler(async (req, res) => {
    * remove secondUserId in user following array and firstUserId in user followers array
    * return message and responce
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
 
 export const removeFollowers = AsyncHandler(async (req, res) => {
   /**
@@ -106,4 +354,35 @@ export const removeFollowers = AsyncHandler(async (req, res) => {
    * remove secondUserId in user followers array and firstUserId in user following array
    * return message and responce
    */
-})
+  // check user login
+  if (!req.userId) {
+    throw new ApiError(400, "LoginError : Not login, please login first");
+  }
+  if (!req.userType) {
+    throw new ApiError(
+      409,
+      "LoginError : UserType not received, pleace clear cookie and login again"
+    );
+  }
+  if (!req.params?.userId) {
+    throw new ApiError(404, "DataError : UserId not received from params");
+  }
+  if (req.params?.userId !== req.userId) {
+    throw new ApiError(409, "ParamsError : Unauthorize Access");
+  }
+  if (!req.params?.userType) {
+    throw new ApiError(404, "DataError : UserType not received from params");
+  }
+  if (req.params?.userType !== req.userType) {
+    throw new ApiError(409, "paramsError : Unauthorize Access");
+  }
+  if (!req.params?.secondUser) {
+    throw new ApiError(
+      404,
+      "DataError : Second userId not received from params"
+    );
+  }
+  if (req.userType === "Employee") {
+  } else if (req.userType === "Candidate") {
+  }
+});
